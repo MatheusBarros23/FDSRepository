6ea7d521746b4fd87de30f1f82af513ba77b2b28
Vitor Carvalho <jvvc@cesar.org.br>
2023-04-26 10:38:59 -0300

    Add tests

--- filme/tests.py
+++ filme/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Documentação selenium para Python: https://selenium-python.readthedocs.io/
 
 # Create your tests here.
+class TestHome(LiveServerTestCase):
+    browser = webdriver.Chrome()
+
+    def test_title(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        assert "The install worked" in self.browser.title
+
+    def test_href(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        link = self.browser.find_element(By.CLASS_NAME, 'logo')
+
+        assert link.get_attribute('href') == 'https://www.djangoproject.com/'
