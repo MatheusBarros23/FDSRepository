2599e237f1ae5015c5398426325c6fcd5a8de487
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-05-10 17:12:12 -0300

    Update tests.py

--- filme/tests.py
+++ filme/tests.py
@@ -4,19 +4,21 @@ from selenium.webdriver.common.by import By
 
 # Documentação selenium para Python: https://selenium-python.readthedocs.io/
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
+
 
 # Create your tests here.
 
 class TestHome(LiveServerTestCase):
     def test_title(self):
-        browser = webdriver.Chrome()
         browser.get('http://127.0.0.1:8000/')
         assert "The install worked" in browser.title
-        browser.quit()
 
     def test_href(self):
-        browser = webdriver.Chrome()
         browser.get('http://127.0.0.1:8000/')
         link = browser.find_element(By.CLASS_NAME, 'logo')
         assert link.get_attribute('href') == 'https://www.djangoproject.com/'
-        browser.quit()
