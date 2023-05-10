686d3ec34ebc501f317fb008cd01ff72ce57bf94
Matheus Barros <88664949+MatheusBarros23@users.noreply.github.com>
2023-05-10 17:13:28 -0300

    Update tests.py

--- filme/tests.py
+++ filme/tests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 # Documentação selenium para Python: https://selenium-python.readthedocs.io/
 # Exemplos: https://ordinarycoders.com/blog/article/testing-django-selenium
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
